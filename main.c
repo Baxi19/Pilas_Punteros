879bd32e2739285ba278808b15f4a2e11e4205a9
Baxi19 <randald1991@gmail.com>
2020-05-06 23:56:35 -0600

    Metodos para agregar y eliminar de la pila trabajando

--- main.c
+++ main.c
@@ -10,13 +10,20 @@
 /*--------------------------------------------------------------------------------------------------------------------*/
 int checkParentheses(char string[MAXCHAR]);
 void loadPila();
+void fillPila();
+void pilaVacia();
+void numeroTopePila();
 
 
 /*--------------------------------------------------------------------------------------------------------------------*/
 int main(){
     /*----------------------------------------------------------------------------------------------------------------*/
-    /*Pila test*/
-    loadPila();
+    //loadPila();
+    //fillPila();
+    //pilaVacia();
+    //numeroTopePila();
+
+
     /*----------------------------------------------------------------------------------------------------------------*/
     /*VARS*/
     char path[MAXCHAR];
@@ -89,12 +96,78 @@ void loadPila(){
     apilar(&pila, 6);
     apilar(&pila, 7);
     apilar(&pila, 8);
-    
+
+    mostrar(&pila);
+
+    apilar(&aux,desapilar(&pila));
+    mostrar(&pila);
+    mostrar(&aux);
+}
+/*--------------------------------------------------------------------------------------------------------------------*/
+void fillPila(){
+    Pila pila, aux;
+    char continuar = 's';
+
+    inicpila(&pila);
+    do{
+        leer(&pila);
+        printf("Para continuar presione s/n");
+        fflush(stdin);
+        scanf("%c", &continuar);
+    }while(continuar == 's');
     mostrar(&pila);
 
 }
-/*--------------------------------------------------------------------------------------------------------------------*/
 
+/*--------------------------------------------------------------------------------------------------------------------*/
+void pilaVacia(){
+    Pila pila, aux;
+    char continuar = 's';
+
+    inicpila(&pila);
+    inicpila(&aux);
+
+    do{
+        leer(&pila);
+        printf("Para continuar presione s/n");
+        fflush(stdin);
+        scanf("%c", &continuar);
+    }while(continuar == 's');
+    mostrar(&pila);
+
+    while(!pilavacia(&pila)){
+        apilar(&aux, desapilar(&pila));
+    }
+    mostrar(&aux);
+}
+/*--------------------------------------------------------------------------------------------------------------------*/
+void numeroTopePila(){
+    Pila pila, aux, basura;
+    char continuar = 's';
+
+    inicpila(&pila);
+    inicpila(&aux);
+    inicpila(&basura);
+
+    do{
+        leer(&pila);
+        printf("Para continuar presione s/n");
+        fflush(stdin);
+        scanf("%c", &continuar);
+    }while(continuar == 's');
+    mostrar(&pila);
+
+    while(!pilavacia(&pila)){
+        if(tope(&pila) == 5){
+            apilar(&basura, desapilar(&pila));
+        }else{
+            apilar(&aux, desapilar(&pila));
+        }
+
+    }
+    mostrar(&basura);
+    mostrar(&aux);
+}
 /*--------------------------------------------------------------------------------------------------------------------*/
 
 
